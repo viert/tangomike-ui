9d3bd7c51dd0750dfc5c9fb35d3962edc4eeb98f
Pavel Vorobyov <aquavitale@yandex.ru>
2023-12-24 16:40:46 +0000

    temporary commit

--- src/views/TrackList.vue
+++ src/views/TrackList.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="track-list-page">
     <header>
-      <h4>Track list</h4>
+      <h5>Track list</h5>
     </header>
     <div class="track-list"></div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useFlightStore } from '@/stores/flights'
+import { useTrackStore } from '@/stores/tracks'
 
-<style scoped></style>
+const store = useTrackStore()
+store.loadFlightList()
+</script>
+
+<style lang="scss">
+.track-list-page {
+  header {
+    padding: 1.25em 1.25em 0.5em;
+    background-color: #f0f0f4;
+    border-bottom: 1px solid #eee;
+    h5 {
+      text-transform: uppercase;
+      font-weight: bold;
+    }
+  }
+
+  .track-list {
+    padding: 1.25em;
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+</style>
